512f40849edfe67ac9d2821d1033b09c09484e55
jesiallen <justjesi2010@gmail.com>
2022-05-07 18:40:00 +0000

    Completed project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [taskList, setTaskList] = useState([]);
+
+	const handleDelete = (i) => {
+		let filtered = taskList.filter((task, index) => i != index);
+		setTaskList(filtered);
+	};
+
+	let listItem = taskList.map((task, index) => {
+		return (
+			<li
+				key={task}
+				className="d-flex justify-content-between"
+				id="parent">
+				{task}
+				<span
+					className="hidden-child"
+					onClick={() => handleDelete(index)}>
+					<i className="fas fa-ban"></i>
+				</span>
+			</li>
+		);
+	});
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="content">
+			<header>ToDo</header>
+			<div>
+				<input
+					type="text"
+					onChange={(e) => setInputValue(e.target.value)}
+					placeholder="What would you like to do?"
+					value={inputValue}
+					onKeyUp={(e) => {
+						if (e.key === "Enter") {
+							setTaskList([...taskList, inputValue]);
+							listItem;
+							setInputValue("");
+						}
+					}}
+				/>
+			</div>
+			<div>
+				<ul>
+					{taskList.length == 0 ? (
+						<li>You do no have any tasks, please add some</li>
+					) : (
+						listItem
+					)}
+				</ul>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
+//heading
+//input field
+//ul
+//li with green check(scratch-thru)complete & trash to delete
